fbdb2879f26dafe8b75a8c51d1133cd30220c6d0
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-28 02:54:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
       }
     }
 		ritchie_language = programmer_hash[:dennis_ritchie][:languages]
-		puts "#{programmer_hash[:dennis_ritchie][:languages]}"
+		puts "ritchie_language"
 end
 
 def adding_matz
